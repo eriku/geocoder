7c401c0f45dd2fd0195bcf1e8e60fa12c0ea608b
Erik Uunila <erik@domain7.com>
2016-02-03 10:07:48 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -34,7 +34,7 @@ app.controller('appCtrl', function($scope, $http) {
     $scope.createUrl = function(url) {
         // var googleAPIKey = 'AIzaSyD-9FWU82CZ3SzpxUNjsZ1Vh6XS5o55uiQ';
         // var googleShortenerUrl = 'https://www.googleapis.com/urlshortener/v1/url?key=' + googleAPIKey;
-        var googleShortenerUrl = 'https://www.googleapis.com/urlshortener/v1/url;
+        var googleShortenerUrl = 'https://www.googleapis.com/urlshortener/v1/url';
         $http
             .post(googleShortenerUrl, {longUrl: url})
             .then(function successCallback(response){
